7258fbb97d1595a95d0cd4dec19a89f967154241
3 Dash <128896582+ColdGuy69@users.noreply.github.com>
2024-08-03 20:26:19 +0800

    About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,13 +8,13 @@ function About() {
           <div className='about'>
             <div className='title'><span>Who I am</span></div><br />
 
-            Hi! My name is <span>Sam</span>, a newbie to the world of coding. I picked up coding to create small applications that align with my personal interests. <br /><br />
+            Hi! My name is <span>Dash</span>, a newbie to the world of coding. I picked up coding to create small applications that align with my personal interests. <br /><br />
 
             Besides coding, a few other things I love to do!<br /><br />
 
             <li>Watching <span>anime</span></li> 
             <li>Reading <span>manga</span> & <span>light novel</span></li> 
-            <li><span>Honkai Impact 3rd</span> & <span>Doki Doki Literature Club</span></li><br />
+            <li><span>Gaming</span> & <span>Youtubing</span></li><br />
 
           </div>
 
@@ -26,8 +26,8 @@ function About() {
           Don't hesitate to get in touch with me<br />
 
           <div className='about-socials'>
-            <a href="https://github.com/kirixenyt" target="_blank"><i class="fa-brands fa-github"></i></a> 
-            <a href="https://www.youtube.com/@kirixenyt" target="_blank"><i class="fa-brands fa-youtube"></i></a> 
+            <a href="https://github.com/1nOnlyDash" target="_blank"><i class="fa-brands fa-github"></i></a> 
+            <a href="https://www.youtube.com/channel/UCUDw_4VfwUHdOW3pn8Z9kmQ" target="_blank"><i class="fa-brands fa-youtube"></i></a> 
           </div>
         </div>
       </div>
