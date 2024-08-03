edb4b9f44181424202974bc1480b3a32eae4a1fd
3 Dash <128896582+ColdGuy69@users.noreply.github.com>
2024-08-03 20:25:00 +0800

    Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,11 @@ function Footer() {
 
     return (
         <footer>
-            <li>Developed by Sam</li>
+            <li>Developed by Dash</li>
             <li>Copyright &#169; 2024</li>
             <div className="footer-socials">
-                <a href="https://github.com/kirixenyt" target="_blank"><i class="fa-brands fa-github"></i></a> 
-                <a href="https://www.youtube.com/@kirixenyt" target="_blank"><i class="fa-brands fa-youtube"></i></a> 
+                <a href="https://github.com/1nOnlyDash" target="_blank"><i class="fa-brands fa-github"></i></a> 
+                <a href="https://www.youtube.com/channel/UCUDw_4VfwUHdOW3pn8Z9kmQ" target="_blank"><i class="fa-brands fa-youtube"></i></a> 
             </div>
         </footer>
     )
